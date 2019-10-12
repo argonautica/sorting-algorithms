a5633195953d26ca21630a0ea4b198fc9fd39b2a
ralouekapoor06 <ralouekapoor1@gmail.com>
2019-10-12 10:46:44 +0530

    Update bubbleSort.js

--- Javascript/bubbleSort.js
+++ Javascript/bubbleSort.js
@@ -2,7 +2,7 @@ const bubbleSort = (numArray) => {
     let rotated;
     do {
       rotated = false;
-      for (let i = 0; i < numArray.length; i++) {
+      for (let i = 0; i < numArray.length-1; i++) {
         if (numArray[i] > numArray[i+1]) {
           store = numArray[i];
           numArray[i] = numArray[i+1];
