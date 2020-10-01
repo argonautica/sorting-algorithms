1b96780b386aaaf14295f67363c88fe3da0ce01f
Swarnabha Roy <scsr2448@gmail.com>
2020-10-01 08:32:16 +0530

    Merge Sort in GO
    This is the implementation of Merge Sort in GoLang.

--- Kotlin/InsertionSort.kt
+++ Kotlin/InsertionSort.kt
@@ -1,55 +1,24 @@
 import java.util.*
 
-fun inputArr(): Array<Int>{
-    //instance of Scanner class
-    val reader: Scanner = Scanner(System.`in`)
-
-    //creating the array
-    println("enter size of array : ")
-    var size:Int=reader.nextInt()
-    var myArray=Array<Int>(size){0}
-
-    //initializing the array
-    for (i in 0 until size-1){
-        print("\nEnter element : ")
-        myArray.set(i,reader.nextInt())
+fun insertionsort(items:MutableList<Int>):List<Int>{
+    if (items.isEmpty() || items.size<2){
+        return items
     }
-
-    return myArray
-}
-
-fun displayArr(myArray: Array<Int>){
-    for (element in myArray)
-        print(" $element")
-}
-
-fun main(args: Array<String>){
-    var array=input()
-    println("array before sorting : ")
-    display(array)
-    array=insertionSort(array)
-    println("\narray after sorting : ")
-    display(array)
-
-}
-
-fun insertionSort(array: Array<Int>): Array<Int> {
-    val n = array.size;
-    for (i in 1 until n-1) {
-        var key = array.get(i);
-        var j = i - 1;
-
-        /* Move elements of arr[0..i-1], that are
-           greater than key, to one position ahead
-           of their current position */
-        while (j >= 0 && array[j] > key) {
-
-            array[j+1] = array[j];
-            j -= 1;
+    for (count in 1..items.count() - 1){
+        // println(items)
+        val item = items[count]
+        var i = count
+        while (i>0 && item < items[i - 1]){
+            items[i] = items[i - 1]
+            i -= 1
         }
-        array[j+1] = key;
+        items[i] = item
     }
-
-    return array
-
+    return items
+}
+fun main(args: Array<String>) {
+    val names = mutableListOf(8, 3, 2, 7, 4)
+    println(names)
+    var ordered = insertionsort(names)
+    println(ordered)
 }
